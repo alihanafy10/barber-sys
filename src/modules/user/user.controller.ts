05eaccf33d10aeecef474994c28339435830500b
Ali Hanafy <engali1030@gmail.com>
2025-03-30 14:46:43 +0200

    v.8

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -156,6 +156,8 @@ async openAndClose(
     @Res() res: Response,
   ): Promise<Response> {
       const authToken = await this.paymobService.getPaymobToken();
+      console.log(authToken);
+      
       const sessionData = await this.paymobService.createOrder(authToken,req);
       return res.status(201).json({ message: 'success', data: sessionData });
   }
